fcbd05922ca52981427f6267dad72e7712e44e0a
AwsSupOps <100229797+AwsSupOps@users.noreply.github.com>
2023-11-22 17:39:29 -0600

    Update next.config.js
    adding basepath

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
  // basePath:'/blogs'/// revoming this path
+ basePath:'/line', /// revoming this path
 }
 
 module.exports = nextConfig
